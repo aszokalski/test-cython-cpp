55e74ef2ff69ecf5d34add8d2a97d3cf0d83334a
Adam Szokalski <szokalskiadam@gmail.com>
2023-11-21 13:48:15 +0100

    xd

--- test.pyx
+++ test.pyx
@@ -1,9 +1,29 @@
-import numpy as np
-class MyClass:
-    def add(self, a, b):
-        return np.add(a, b)
-    def sub(self, a, b):
+import cppyy
+cppyy.cppdef(
+"""
+    class MyClass
+{
+public:
+    MyClass(){
+
+    }
+    ~MyClass(){
+
+    }
+
+    int add(int a, int b){
+        return a + b;
+    }
+};
+"""
+)
+
+from cppyy.gbl import MyClass
+
+class MyClassEdited(MyClass):
+    def add(a, b):
+        # sub
         return a - b
 
 cdef public object createMyClass():
-    return MyClass()
+    return MyClassEdited()
